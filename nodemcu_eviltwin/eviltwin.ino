e688e6cef2034f9cc447f5a9dd67299b3cb42740
Ishinder Singh <78218217+ishu22ji@users.noreply.github.com>
2024-02-14 12:32:01 +0530

    Add files via upload

---
+++ nodemcu_eviltwin/eviltwin.ino
@@ -0,0 +1,28 @@
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  Serial.begin(9600);
+  mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
+}
+
+void loop() {
+  if (mySwitch.available()) {
+    int value = mySwitch.getReceivedValue();
+
+    if (value == 0) {
+      Serial.println("Unknown encoding");
+    } else {
+      Serial.print("Received ");
+      Serial.print(value);
+      Serial.print(" / ");
+      Serial.print(mySwitch.getReceivedBitlength());
+      Serial.print("bit ");
+      Serial.print("Protocol: ");
+      Serial.println(mySwitch.getReceivedProtocol());
+    }
+
+    mySwitch.resetAvailable();
+  }
+}
